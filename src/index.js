b48de98d66fc405d079648ac0938703932e8cb44
Vicki Edwards <vicki-tamar@hotmail.com>
2018-03-13 15:00:13 +0000

    basic view

--- src/index.js
+++ src/index.js
@@ -8,31 +8,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 // import in Immutable Map and List for our dummy data
-import { Map, List } from "immutable";
 
-// create some dummy articles
-const articles = List([
-    Map({
-        id: 1,
-        title: "Post #1",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "bob@bob.com", comment: "Great blog post" }),
-        ]),
-        tags: List(["blah", "monkeys"]),
-    }),
-    Map({
-        id: 2,
-        title: "Post #2",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" }),
-        ]),
-        tags: List(["blah", "fish-fingers"]),
-    }),
-]);
 
-// wrap the app in Router
+import store from "./data/store";
+import initial from "./data/initial";
+
+
+
+let articles = store.getState().get("articles");
 // pass our articles through to App using props
 ReactDOM.render(
     <Router>
